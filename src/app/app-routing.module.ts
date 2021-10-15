3e9dcfa03c031f182c55ff5e62fb95dc295bb632
Luc Anise <lca11@me.com>
2021-10-15 14:14:51 -0400

    Fixed login module placement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'signup', loadChildren: () => import('./modules/signup/signup.module').then(m => m.SignupModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }];
+const routes: Routes = [
+  {
+    path: 'signup',
+    loadChildren: () =>
+      import('./modules/signup/signup.module').then((m) => m.SignupModule),
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./modules/login/login.module').then((m) => m.LoginModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
-
+export class AppRoutingModule {}

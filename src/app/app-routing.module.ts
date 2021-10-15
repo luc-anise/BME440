4cb1e20cd0c03f6fbf32bb811cf3ef7dfa47fa2b
Alex Eichert <alexander.eichert@stonybrook.edu>
2021-10-15 14:09:08 -0400

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'signup', loadChildren: () => import('./modules/signup/signup.module').then(m => m.SignupModule) }];
+const routes: Routes = [{ path: 'signup', loadChildren: () => import('./modules/signup/signup.module').then(m => m.SignupModule) }, { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

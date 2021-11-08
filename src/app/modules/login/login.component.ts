7a3c0767d14de3272792ca71387255fe933bf00d
Luc Anise <lca11@me.com>
2021-11-08 14:53:04 -0500

    Update login.component.ts

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,14 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  signInWithEmailAndPassword,
+  Auth,
+  AuthError,
+} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router, private auth: Auth) {}
 
   ngOnInit(): void {}
 
+  async signIn() {
+    try {
+      await signInWithEmailAndPassword(
+        this.auth,
+        'luc.anis@stonybrook.edu',
+        'password1'
+      );
+      console.log('User signed in sucessfully!');
+      this.router.navigate(['/', 'dashboard']);
+    } catch (error) {
+      let err = error as AuthError;
+      console.error(err);
+    }
+  }
 }

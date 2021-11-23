12f57a2d21fe69dd2331d8734d932653e8e70694
Alex Eichert <alexander.eichert@stonybrook.edu>
2021-11-22 20:23:41 -0500

    case details and garbo

--- src/app/modules/simulation/components/case-details/case-details.component.ts
+++ src/app/modules/simulation/components/case-details/case-details.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CaseDetailsComponent implements OnInit {
 
+  history = ["Case History: LVAD patient DH (58YO male) was presented to the Emergency Department after he jumped in the pool to save his son who he thought was drowning","test","yee"]
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/modules/simulation/simulation.component.ts
+++ src/app/modules/simulation/simulation.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simulation.component.scss']
 })
 export class SimulationComponent implements OnInit {
+  
+  history = ["Case History: LVAD patient DH (58YO male) was presented to the Emergency Department after he jumped in the pool to save his son who he thought was drowning","test","yee"]
 
   constructor() { }
 

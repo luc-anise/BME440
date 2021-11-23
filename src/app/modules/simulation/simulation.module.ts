44ef6df094e30b4d67eb0016c3473aafdfa547db
Luc Anise <lca11@me.com>
2021-11-22 20:50:49 -0500

    Added Feedback Component

--- src/app/modules/simulation/simulation.module.ts
+++ src/app/modules/simulation/simulation.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { SimulationRoutingModule } from './simulation-routing.module';
 import { SimulationComponent } from './simulation.component';
 import { CaseDetailsComponent } from './components/case-details/case-details.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
 
 
 @NgModule({
   declarations: [
     SimulationComponent,
-    CaseDetailsComponent
+    CaseDetailsComponent,
+    FeedbackComponent
   ],  
   imports: [
     CommonModule,

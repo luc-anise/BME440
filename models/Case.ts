0aaaec7a560ff9d731c7046c3fd9bb94d36defc7
Luc Anise <lca11@me.com>
2021-11-22 19:49:23 -0500

    Finished Initial Scaffold

--- models/Case.ts
+++ models/Case.ts
@@ -1,14 +1,51 @@
-
 interface Case {
-    name: string;
-    vitals: Vitals;
+  name: string;
+  vitals: Vitals;
+  controller: Controller;
+  lvadTeam: LVADTeam;
+  //Labs: Labs; -- Not using this yet
 }
 
 interface Vitals {
-    bloodPressure: BloodPressure;
-    doppler: string;
+  bloodPressure: BloodPressure;
+  heartRate: CasePossibility;
+  temperature: CasePossibility;
+  pulseOximeter: CasePossibility;
+  Respirations: CasePossibility;
+}
+interface Controller {
+    display: {
+        parameters: CasePossibility;
+        arrowCheck: CasePossibility;
+    }
+    listenForHum: CasePossibility;
+    pump: {
+        isOn: {
+            haveLVADTeamReplaceController: CasePossibility;
+            replaceControllerYourself: CasePossibility;
+        }
+        isOff: {
+            haveLVADTeamReplaceController: CasePossibility;
+            replaceControllerYourself: CasePossibility;
+        }
+    }
+}
+interface LVADTeam {
+    callCoordinator: CasePossibility;
+}
+interface BloodPressure {
+  NIBPcuff: CasePossibility;
+  doppler: CasePossibility;
 }
 
-interface BloodPressure {
-    NIBPcuff: string;
+interface Labs {
+  CBC: CasePossibility;
+}
+interface CasePossibility {
+  details: string;
+  feedback: string;
+  checkedByUser: boolean;
+  requiredToCheck: boolean;
+  criticalFailure: boolean;
+  resolvesSimulation: boolean;
 }

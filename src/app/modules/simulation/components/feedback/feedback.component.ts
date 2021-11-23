dd41a2102743a28e860b3b145f659e8d0588aebb
Luc Anise <lca11@me.com>
2021-11-23 12:36:15 -0500

    added feedback binding

--- src/app/modules/simulation/components/feedback/feedback.component.ts
+++ src/app/modules/simulation/components/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CaseService } from 'src/app/services/case.service';
 
 @Component({
   selector: 'app-feedback',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackComponent implements OnInit {
 
-  constructor() { }
+  constructor(public caseService: CaseService) { }
 
   ngOnInit(): void {
+    this.caseService.addFeedback('Test Feedback')
   }
 
 }

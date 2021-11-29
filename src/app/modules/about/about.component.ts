0de9b50432ca7d28aa7c56c85322af6813194ece
Alex Eichert <alexander.eichert@stonybrook.edu>
2021-11-29 18:13:25 -0500

    aboutus

--- src/app/modules/about/about.component.ts
+++ src/app/modules/about/about.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
 
    names = ["Luc Annise", "Alexander Eichert", "Shayne Hays", "Sarah Spinelli", "Lydia Sprague"]
+   msnames = ["Aidan Aicher, MS4", "Alex Freedenberg, MS4", "Sara Kianian, MS3", "Gloria Singleton, MS3"]
+
   constructor() { }
 
   ngOnInit(): void {

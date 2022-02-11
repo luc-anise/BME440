9af6ea3981de6c55172b204b25bf9eed89706d8c
Alex Eichert <alexander.eichert@stonybrook.edu>
2022-02-10 21:34:36 -0500

    case 2

--- models/Case.ts
+++ models/Case.ts
@@ -59,7 +59,7 @@ export let Case1: Case = {
   name: 'Case 1',
   details: 'SYSTEM CONTROLLER FAULT ALARM',
   givenInformation:
-    'Case History: LVAD patient DH (58YO male) was presented to the Emergency Department after he jumped in the pool to save his son who he thought was drowning',
+    'Case History: LVAD patient DH (58 YO male) was presented to the Emergency Department after he jumped in the pool to save his son who he thought was drowning',
   controller: {
     display: {
       parameters: {
@@ -186,6 +186,140 @@ export let Case1: Case = {
       resolvesSimulation: false,
     },
   },
+};
+
+export let Case2: Case = {
+  name: 'Case2',
+  details: 'SYSTEM CONTROLLER FAULT ALARM',
+  givenInformation:
+    'Case History: Patient is a 74 YO M with a PMH of Class IV CHF (with LVAD implantation two years ago). Patient presented to the ED following his husband noticing that the patient was slurring their speech and their face looked "funny" on the left side. EMS reported left sided pronator defit and decreased awareness (AOx2). Patient vitals from EMS reported HLD and HTN (160/83). Patient medication list includes sacubitril, valsartan, atorvastatin, metoprolol, furosemide, sprionolactone, aspirin, and warfarin. ',
+  controller: {
+    display: {
+      parameters: {
+        details: 'Parameters',
+        id: 'parameters',
+        feedback: 'LVAD parameters showed low PI, elevated power, low flow, and normal speed.',
+        requiredToCheck: false,
+        criticalFailure: false,
+        resolvesSimulation: false,
+      },
+      arrowCheck: {
+        id: 'arrowCheck',
+        details: 'Arrow check',
+        feedback: 'Arrows ON',
+        requiredToCheck: false,
+        criticalFailure: false,
+        resolvesSimulation: false,
+      },
+    },
+    listenForHum: {
+      id: 'listenForHum',
+      details: 'Listen for hum',
+      feedback: 'There is an audible hum',
+      requiredToCheck: false,
+      criticalFailure: false,
+      resolvesSimulation: false,
+    },
+    pump: {
+      isOn: {
+        haveLVADTeamReplaceController: {
+          id: 'pumpOnVADReplace',
+          details: 'Pump is on: Have VAD team replace controller',
+          feedback: '',
+          requiredToCheck: false,
+          criticalFailure: false,
+          resolvesSimulation: false,
+        },
+        replaceControllerYourself: {
+          id: 'pumpOnYouReplace',
+          details: 'Pump is on: Replace controller yourself immediately',
+          feedback: 'Dead',
+          requiredToCheck: false,
+          criticalFailure: true,
+          resolvesSimulation: false,
+        },
+      },
+      isOff: {
+        haveLVADTeamReplaceController: {
+          id: 'pumpOffVADReplace',
+          details: 'Pump is off: Have VAD team replace controller',
+          feedback: '',
+          requiredToCheck: false,
+          criticalFailure: true,
+          resolvesSimulation: false,
+        },
+        replaceControllerYourself: {
+          id: 'pumpOffYouReplace',
+          details: 'Pump is off: Replace controller yourself immediately',
+          feedback: 'Dead',
+          requiredToCheck: false,
+          criticalFailure: true,
+          resolvesSimulation: false,
+        },
+      },
+    },
+  },
+  vitals: [
+    {
+      id: 'NIBPCuff',
+      details: 'NIBP cuff',
+      feedback: 'You are unable to obtain an accurate reading.',
+      requiredToCheck: true,
+      criticalFailure: false,
+      resolvesSimulation: false,
+    },
+    {
+      id: 'doppler',
+      details: 'Doppler',
+      feedback: 'Doppler shows a mean arterial pressure of 100 mmHg.',
+      requiredToCheck: true,
+      criticalFailure: false,
+      resolvesSimulation: false,
+    },
+    {
+      details: 'Heart Rate',
+      id: 'heartRate',
+      feedback: 'There is no measurable heart rate',
+      requiredToCheck: true,
+      criticalFailure: false,
+      resolvesSimulation: false,
+    },
+    {
+      id: 'temperature',
+      details: 'Temperature',
+      feedback: 'Patient temperature is 38.4 deg C.',
+      requiredToCheck: true,
+      criticalFailure: false,
+      resolvesSimulation: false,
+    },
+    {
+      id: 'pulseOximeter',
+      details: 'Pulse Oximeter',
+      feedback: 'O2 error: no reading',
+      requiredToCheck: true,
+      criticalFailure: false,
+      resolvesSimulation: false,
+    },
+    {
+      id: 'respirations',
+      details: 'Respirations',
+      feedback: 'Respiration rate is within normal range',
+      requiredToCheck: true,
+      criticalFailure: false,
+      resolvesSimulation: false,
+    },
+  ],
+  lvadTeam: {
+    callCoordinator: {
+      id: 'callLVADCoordinator',
+      details: 'Call the LVAD Coordinator',
+      feedback: 'VAD Team has been called',
+      requiredToCheck: true,
+      criticalFailure: false,
+      resolvesSimulation: false,
+    },
+  },
+  
 };
 
 export let key: string[] = [

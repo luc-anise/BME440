88e3bccb4ebf41042d5fe5b3516d28a595999081
Luc Anise <lca11@me.com>
2022-02-11 17:04:10 -0500

    copied existing button logic into case-button component

--- src/app/modules/simulation/components/case-button/case-button.component.ts
+++ src/app/modules/simulation/components/case-button/case-button.component.ts
@@ -1,18 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ChildActivationStart } from '@angular/router';
 import { CasePossibility } from 'models/Case';
+import { CaseService } from 'src/app/services/case.service';
 
 @Component({
   selector: 'app-case-button',
   templateUrl: './case-button.component.html',
-  styleUrls: ['./case-button.component.scss']
+  styleUrls: ['./case-button.component.scss'],
 })
 export class CaseButtonComponent implements OnInit {
+  @Input() casePosiblity: CasePossibility = {
+    id: '',
+    details: '',
+    feedback: '',
+    requiredToCheck: false,
+    criticalFailure: false,
+    resolvesSimulation: false,
+  };
 
-  @Input() CasePosiblity: CasePossibility;
+  constructor(private caseService: CaseService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  buttonClicked(){
+   this.caseService.handleCasePossiblity(this.casePosiblity)
   }
 
 }

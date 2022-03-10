447ef142060086ddb4b1a68acab98f0ec2644459
Luc Anise <lca11@me.com>
2022-03-09 19:22:18 -0500

    Added Attempt Model

---
+++ models/Attempt.ts
@@ -0,0 +1,11 @@
+import { Timestamp } from '@angular/fire/firestore';
+import { CasePossibility } from './Case';
+export interface Attempt {
+  id?: string;
+  caseID: string;
+  userID: string;
+  startTime: Timestamp;
+  endTime: Timestamp;
+  selectedCasePossibilities: CasePossibility[];
+  score: number;
+}

e03afb808aa1bb93c70683883d42beb0ae9743a3
Luc Anise <lca11@me.com>
2022-03-10 17:15:27 -0500

    Updated Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/dashboard/dashboard.module').then(
         (m) => m.DashboardModule
-      )
+      ),
   },
   {
     path: 'simulation',
